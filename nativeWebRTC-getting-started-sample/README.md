ec257f796a9863debae139d0839a1313cf09c7bb
Kapil Khurana <kapil.khurana@rsystems.com>
2023-12-13 10:38:42 +0530

    have done web app changes and middleware service url in index.html & main.js

---
+++ nativeWebRTC-getting-started-sample/README.md
@@ -0,0 +1,66 @@
+
+<!-- ABOUT THE PROJECT -->
+# About VidyoPlatform
+
+Leverage VidyoPlatform to build your own video conferencing application with just a few lines of code. We provide a simplified, real-time video interaction for multiple deployment models including on-premises, hybrid, private cloud, and multi-tenant cloud.
+
+We have created sample code which demonstrates how it works and how easy it is to integrate our solution to get started with your own video conferencing.
+
+This sample application aims to achieve the following:
+
+  **- Evaluate Vidyo Platform SDK unmatched video conferencing quality**
+
+  **- Demonstrates how easy it is to integrate Vidyo’s SDK into your solution and have an embedded in brand robust video communication capability.**
+  
+  Contact us at https://www.vidyo.com/company/contact-vidyo to learn more or tell us what you think.
+
+Happy Coding.
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+
+<!-- HOW TO RUN THE DEMO -->
+# How to run the demo
+
+Do the following from a terminal window:
+
+1. Clone the repo
+   ```sh
+   git clone https://github.com/VidyoAPI-SDK/nativeWebRTC-getting-started-sample.git
+
+2. Navigate to the project directory and run the following command
+   ```sh
+   npm start
+   ```
+ This will start a local http server. Note the URL, which is  usually  http://127.0.0.1:8080 or http://127.0.0.1:8081
+
+3. Open the http server URL from the step above in your browser. In the browser pop-up, allow the  page access to audio and video.
+
+![Alt text](allowMedia.png?raw=true)
+
+4. Click the 'start call' button. This will start a video call in a private room. 
+
+![Alt text](startCall.png?raw=true)
+
+
+5. After a call is started, a meeting link is generated. This link can be shared with others who can join your call from their browser using the link.
+
+![Alt text](meetingLink.png?raw=true)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+<!-- CONTACT -->
+# Contact
+
+Email: support@vidyocloud.com
+
+Company Link: https://www.vidyo.com/company/contact-vidyo
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+
---
+++ nativeWebRTC-getting-started-sample/main.js
@@ -0,0 +1,94 @@
+import { VC } from 'https://cdn.jsdelivr.net/npm/vidyoclient-nativewebrtc-sdk';
+// declare global variables
+let vidyoConnector = null;
+const startTooltip = document.getElementById('startTooltip');
+const loadingPopUp = document.getElementById('loadingPopUp');
+const meetingLink = document.getElementById('meetingLink');
+const startBtn = document.getElementById('btnStart');
+const endBtn = document.getElementById('btnEnd');
+const copyBtn = document.getElementById('copyBtn');
+const name = document.getElementById('name');
+const roomname = document.getElementById('roomname');
+
+async function init() {
+    try {
+        vidyoConnector = await VC.CreateVidyoConnector({
+            viewId: "renderer", // Div ID where the composited video will be rendered, see VidyoConnector.html;
+            viewStyle: "VIDYO_CONNECTORVIEWSTYLE_Default", // Visual style of the composited renderer
+            remoteParticipants: 8,     // Maximum number of participants to render
+            logFileFilter: "debug@VidyoClient debug@VidyoSDP debug@VidyoResourceManager",
+            logFileName: "",
+            userData: 0,
+            constraints: {}
+        });
+        console.log("create success");
+        startTooltip.classList.add('active')
+    } catch (error) {
+        console.error('creating failed', error);
+    }
+}
+
+async function joinCall() {
+    try {
+        if (!name.checkValidity()) {
+            return name.reportValidity();
+        }
+        startBtn.disabled = true;
+        startTooltip.classList.remove('active');
+        loadingPopUp.setAttribute('data-text', 'Creating a room...');
+        // create new room
+		let res = await fetch('http://4.144.177.228:80/searchRooms?roomName='+roomname.value, {method: 'POST'});			
+        res = await res.json();
+        console.log(res);
+        const params = res.roomUrl.split('/join/');
+        const portal = params[0];
+        const roomKey = params[1];
+
+        loadingPopUp.setAttribute('data-text', 'Joining a call...');
+        await vidyoConnector.ConnectToRoomAsGuest({
+            host: portal, // HOST
+            roomKey: roomKey, //ROOM KEY
+            displayName: name.value,
+            roomPin: res.pin,
+            onSuccess: () => {
+                console.log(`vidyoConnector.ConnectToRoomAsGuest : onSuccess callback received`);
+                meetingLink.textContent = res.roomUrl;
+                loadingPopUp.setAttribute('data-text', '');
+                document.body.classList.add('in-call');
+                startBtn.disabled = false;
+            },
+            onFailure: (reason) => {
+                console.error("vidyoConnector.Connect : onFailure callback received", reason);
+                handleDisconnect();
+            },
+            onDisconnected: (reason) => {
+                console.log("vidyoConnector.Connect : onDisconnected callback received", reason);
+                handleDisconnect();
+            }
+        });
+    } catch(error) {
+        console.log(error);
+        handleDisconnect();
+    }
+}
+
+function endCall() {
+    vidyoConnector.Disconnect();
+}
+
+function handleDisconnect() {
+    loadingPopUp.setAttribute('data-text', '');
+    document.body.classList.remove('in-call');
+    meetingLink.textContent = '';
+    startBtn.disabled = false;
+}
+
+function copyToClipboard() {
+    navigator.clipboard.writeText(meetingLink.textContent);
+}
+
+startBtn.onclick = () => joinCall();
+endBtn.onclick = () => endCall();
+copyBtn.onclick = () => copyToClipboard();
+
+init();
